492530bd7cebd5189f12e467054d39c5ec45cf74
reem_mustafa <reemmustafa905@gmail.com>
2025-10-11 22:09:47 +0300

    Navigating between Pages

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,10 @@
+import Navigation from "../components/Navigation";
+
 export default function Page() {
-  return <h1>Cabin Page</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>Cabin Page</h1>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>The Wild Oasis Website.welcome to paradise</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>The Wild Oasis Website.welcome to paradise</h1>
+      <Link href="/cabins">Explore luxury cabins</Link>;
+    </div>
+  );
 }
