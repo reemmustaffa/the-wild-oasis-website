fd6419d0dce632708907d50b5cbc11ffe43f7c6d
reem_mustafa <reemmustafa905@gmail.com>
2025-10-13 17:23:13 +0300

    fetching data in the page using RSC

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,16 @@
-import Navigation from "../components/Navigation";
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
 
-export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Cabin Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
