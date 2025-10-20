4d2bc898a621f5a1a574c302ea70283a5736b207
reem_mustafa <reemmustafa905@gmail.com>
2025-10-21 00:57:56 +0300

    streaming route segments with loading.js file.

---
+++ app/cabins/loading.js
@@ -0,0 +1,11 @@
+import Spinner from "@/app/_components/Spinner";
+
+//دا خاص بكابين بيدج
+export default function Loading() {
+  return (
+    <div className="grid items-center justify-center">
+      <Spinner />
+      <p className="text-xl  text-primary-200">loading cabin data..</p>
+    </div>
+  );
+}
