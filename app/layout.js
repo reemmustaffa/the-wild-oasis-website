0ffab3a5940ce2d39c5fe9a40d6242c1a51ed77e
reem_mustafa <reemmustafa905@gmail.com>
2025-10-20 12:24:32 +0300

    styling with Tailwind CSS

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -10,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
