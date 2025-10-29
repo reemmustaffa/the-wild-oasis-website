5c5fb1d5e8e5f0e25a233527ce90be3135720d3f
reem_mustafa <reemmustafa905@gmail.com>
2025-10-29 23:57:00 +0300

    data fetching stratigies for Reservation section

---
+++ app/_components/Reservation.js
@@ -0,0 +1,24 @@
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+async function Reservation({ cabin }) {
+  //الفكره هنا كأنهم ماشين بالتوازي يعني مفيش حاجه هتستني التانيه
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+      <DateSelector
+        settings={settings}
+        cabin={cabin}
+        bookedDates={bookedDates}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
+
+export default Reservation;
