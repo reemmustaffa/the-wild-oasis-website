42229b96e300cd7b80111c754759c8b840b7d69e
reem_mustafa <reemmustafa905@gmail.com>
2025-10-29 14:49:44 +0300

    sharing state between client and server :The URl

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 //دي معاناها هيحصل ريفتش للداتا كل مره بعمل ريكوست فيها
 // export const revalidate = 0;
@@ -12,7 +13,9 @@ export const revalidate = 3600;
 export const metadata = {
   title: "Cabins",
 };
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -27,8 +30,12 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
