33e0affddc0ed30e0e2a1ce26c73b325175b8915
reem_mustafa <reemmustafa905@gmail.com>
2025-11-17 15:09:43 +0200

    creating a new guest in first sign in

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -13,6 +14,21 @@ const authConfig = {
     authorized({ auth, request }) {
       //دي بتحول الاسترنج لترو وفولس بدل ماعمل شرط طويل
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest)
+          await createGuest({ fullName: user.name, email: user.email });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
 
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,2 @@
+//“أي request يجي على /api/auth/... هيتعامل عن طريق NextAuth.
 export { GET, POST } from "@/app/_lib/auth";
