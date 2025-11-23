b7962824ac892bdd843464d21dd1d8ef6d1ee382
reem_mustafa <reemmustafa905@gmail.com>
2025-11-23 14:59:53 +0200

    deleting the reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -23,6 +24,29 @@ export async function updateGuest(formData) {
   // Revalidate the profile page to reflect the updated data
   //عد تحميل الصفحة أو المسار ده، لأن البيانات اتغيرت
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("you must be logged in first");
+
+  //to prevent deleting the bookings of other uers
+
+  const guestBookings = await getBookings(session.user.guestId);
+
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+
+  revalidatePath("account/reservations");
 }
 
 export async function signInAction() {
